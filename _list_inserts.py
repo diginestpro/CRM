052fcaaf745cc218c432a8d05be302f5cd383e3d
diginestpro <diginestpro@gmail.com>
2026-09-08 18:11:32 +0500

    chore: remove leftover helper scripts

--- _list_inserts.py
+++
@@ -1,7 +0,0 @@
-with open(r'C:\Users\LENOVO\Desktop\CRM\lib\payments.ts','rb') as f: c = f.read()
-import re
-for m in re.finditer(rb'from\("payment_transactions"\)', c):
-    pos = m.start()
-    print(f'At byte {pos}:')
-    print(repr(c[pos:pos+250]))
-    print('---')
--- _show_insert.py
+++
@@ -1,8 +0,0 @@
-with open(r'C:\Users\LENOVO\Desktop\CRM\lib\payments.ts','rb') as f:
-    c = f.read()
-import re
-m = re.search(rb'await supabase\.from\("payment_transactions"\)\.insert\(\{[^}]+\}\)[^/]*', c, re.DOTALL)
-if m:
-    print(repr(m.group()))
-else:
-    print('NO MATCH')
